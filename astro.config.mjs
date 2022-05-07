d80770c50b38c504d30eafbddededbc8d30de257
Ashwin Gupta <cooldeveloper101@gmail.com>
2022-05-07 14:05:04 +0530

    Remove react dependency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
 
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'ignore',
-  integrations: [react(), deno()]
+  integrations: [deno()]
 });
